8e93ef6503b63edb0ad6b3202ecfabc5eab33e70
olenkasereda <106010289+olenkasereda@users.noreply.github.com>
2022-10-21 11:48:59 +0300

    changed files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,21 +9,32 @@ const onPlay = function (data) {
   localStorage.setItem(STORAGE_KEY, data.seconds);
   console.log(data.seconds);
 };
+player.on('loaded', function () {
+  /*найкраще використовувати встановлення часу в момент коли відео завантажене*/
+  if (localStorage.getItem(STORAGE_KEY)) {
+    // player.setCurrentTime(seconds);
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+  }
+});
+/*у випадку якщо записувати при події play часто відео запускається з часу в 
+localStorage якщо перемотувати  збереження часу*/
 
 player.on('timeupdate', throttle(onPlay, 1000));
-player
-  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+//===============================================
+// player
+//   .setCurrentTime(localStorage.getItem(STORAGE_KEY))
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,82 @@
-import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle'; // подключили библиотеку throttle
 
 const form = document.querySelector('.feedback-form'); // отримали доступ до форми
-
 const LOCALSTORAGE_KEY = 'feedback-form-state'; // ключ для збереження даних
-let selectedForm = {};
 
-form.addEventListener('input', throttle(saveMessage, 500));
+/**
+  |============================
+  | вариант без гобальной переменной
+  |============================
+*/
+form.addEventListener('input', throttle(saveMessage, 500)); // слушатель событий input на всю форму
 
-updateOutput();
+updateOutput(); // вызов фу-ции обновить вывод (инициализируем форму    )
 
 function saveMessage(evt) {
-  selectedForm[evt.target.name] = evt.target.value;
-  console.log(selectedForm);
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(selectedForm));
-  //form.reset();
+  //вся робота по проверке localStorage закрыта в этой ф-ии
+  //ф=ция сохронить ввод
+  let persistedForm = localStorage.getItem(LOCALSTORAGE_KEY); // перем-я сохраняемая форма, читаем с ЛХ запись с ключем LOCALSTORAGE_KEY
+  persistedForm = persistedForm ? JSON.parse(persistedForm) : {}; //тернарник: если в хранилище есть запись мы его дастаем перед этим parse, если нет- создаем пустой обьект
+  persistedForm[evt.target.name] = evt.target.value; //записываем name и value изменяя в старых заптсях или вносим первый раз в пустой обьект
+  console.log(persistedForm);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(persistedForm)); //опять сохранили новую запись в ЛХ
 }
 
 function updateOutput() {
-  let persistedForm = localStorage.getItem(LOCALSTORAGE_KEY);
+  // фу-ция обновить вывод, тт значение из ЛХ  хотим записать после обновления в инпуты
+  let persistedForm = localStorage.getItem(LOCALSTORAGE_KEY); //смотрим что есть в ЛХ  с ключем LOCALSTORAGE_KEY
   if (persistedForm) {
-    persistedForm = JSON.parse(persistedForm);
+    // если что-то есть, то
+    persistedForm = JSON.parse(persistedForm); //парсим то что есть (из строки в обьект)
     Object.entries(persistedForm).forEach(([name, value]) => {
-      selectedForm[name] = value;
-      form.elements[name].value = value;
-    });
+      //перебераем через forEach все entries (песле деструктуризации это [name, value] обьекта persistedForm)
+      form.elements[name].value = value; // свойство elements создает псевдоколлекцию( на form обращаемся к елесенту с именем name в его value записываем новое-текущее значения value в инпуте)
+    }); // если в persistedForm ничего нету -ничего не делаем
     console.log(persistedForm);
   }
 }
 
 form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  form.reset();
-  selectedForm = {};
-  localStorage.removeItem(LOCALSTORAGE_KEY);
-  console.log(selectedForm);
+  // вешай слушателя на кнопку submit
+  evt.preventDefault(); //збрасываем стандапртые настройки браузера
+  form.reset(); //reset() восстанавливает стандартные значения всем элементам формы.
+  localStorage.removeItem(LOCALSTORAGE_KEY); //удаляем из ЛХ сущест-ю запись
 });
+
+/**
+  |============================
+  | вариант з глобюальною переменной
+  |============================
+*/
+// let selectedForm = {}; //// это обьект в кторый буду записывать значения которые я ввела в инпут
+
+// form.addEventListener('input', throttle(saveMessage, 500));
+
+// updateOutput();
+
+// function saveMessage(evt) {
+//   selectedForm[evt.target.name] = evt.target.value; ////в пустой обьект записываем имя и значение, или презаписываем новые если старое есть значение
+//   console.log(selectedForm);
+//   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(selectedForm)); ////записыввем изменнеия нашего обьекта selectedForm в localStorage
+//   //form.reset();
+// }
+
+// function updateOutput() {
+//   let persistedForm = localStorage.getItem(LOCALSTORAGE_KEY);
+//   if (persistedForm) {
+//     persistedForm = JSON.parse(persistedForm);
+//     Object.entries(persistedForm).forEach(([name, value]) => {
+//       selectedForm[name] = value; //наполяняем ноши импуты изначально, чтоб не обнулялись, ведь глобально обьект пустой
+//       form.elements[name].value = value;
+//     });
+//     console.log(persistedForm);
+//   }
+// }
+
+// form.addEventListener('submit', evt => {
+//   evt.preventDefault();
+//   form.reset();
+//   selectedForm = {}; ////каждый раз очищаем обьект при submit
+//   localStorage.removeItem(LOCALSTORAGE_KEY);
+//   console.log(selectedForm);
+// });
